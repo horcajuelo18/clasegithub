d777940f0a26c57a8ac25b63effe23d0b7399707
Alex <aecadro@gmail.com>
2026-05-22 18:24:29 +0200

    commit 1

---
+++ ejercicioGithub.py
@@ -0,0 +1,11 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        return f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años."
+    
+# Crear una instancia de la clase Persona
+
+# saludar
